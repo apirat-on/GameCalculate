e7b07e5a998a6deda1c5a43754ea5fada8cc6e3b
Piyathep <pmg_1037@hotmail.com>
2018-01-09 15:44:18 +0700

    Back Controller

--- app/src/main/java/piyathep/krirk/ac/th/gamecalculate/fragment/CalculateFragment.java
+++ app/src/main/java/piyathep/krirk/ac/th/gamecalculate/fragment/CalculateFragment.java
@@ -4,10 +4,12 @@ package piyathep.krirk.ac.th.gamecalculate.fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import piyathep.krirk.ac.th.gamecalculate.MainActivity;
 import piyathep.krirk.ac.th.gamecalculate.R;
 
 /**
@@ -16,10 +18,37 @@ import piyathep.krirk.ac.th.gamecalculate.R;
 
 public class CalculateFragment extends Fragment {
 
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+//        Create Toolbar
+        createToolbar();
+
+    }   // Main Method
+
+    private void createToolbar() {
+        Toolbar toolbar = getView().findViewById(R.id.toolbarCalculate);
+        ((MainActivity)getActivity()).setSupportActionBar(toolbar);
+
+        ((MainActivity) getActivity()).getSupportActionBar().setTitle("Calculate");
+
+        ((MainActivity) getActivity()).getSupportActionBar().setHomeButtonEnabled(true);
+        ((MainActivity) getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getActivity().getSupportFragmentManager().popBackStack();
+            }
+        });
+    }
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_calculate, container, false);
         return view;
     }
+
+
 }   // Main Class
